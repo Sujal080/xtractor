85f01f677dd9e3ee36ac237213ed3cf4ada2277d
SUJAL CHAVDA <chavdasujal08@gmail.com>
2025-07-18 12:16:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config(object):
+    
+    BOT_TOKEN = "8012231272:AAFITViiSvq-2hXqbCaS4VBwHahNsim-woE"
+    
+    API_ID = 12475131
+    
+    API_HASH = "719171e38be5a1f500613837b79c536f"
+    ADMIN_ID = [12475131]
+    
+    DB_URL = "mongodb+srv://tmglcd:kI1UijMr2jJXyOXY@cluster0.unjzi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    
+    DB_NAME = "MY_BOT_DB"
+    
+    TXT_LOG = -1002542634912
+    
+    AUTH_LOG = -1002542634912
+    
+    HIT_LOG = -1002542634912
+    
+    DRM_DUMP = -1002542634912
+    # Main channel ki ID
+    CHANNEL = -1002542634912
+    # Channel ka link
+    CH_URL = "https://t.me/gjskisb"
+    
+    OWNER = "https://t.me/gjskisb"
+    
+    THUMB_URL = "https://tinypic.host/images/2025/02/07/IMG_20250207_224444_975.jpg" #Replace by with your Thumb URL
+   
+    HOST = "https://api.masterapi.tech"
